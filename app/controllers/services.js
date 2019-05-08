ac40d72844061ecd0cdf32eed50d6dd762ec251d
pisomojadev <nbdavis.stl@gmail.com>
2019-05-07 19:36:37 -0700

    creating controller placeholder

---
+++ app/controllers/services.js
@@ -0,0 +1,5 @@
+'use strict'
+
+// place mapped endpoint functions here, can also place helpers although
+// i think it's better to split them out into their own files for unit
+// testing purposes.
--- app/routes/services.js
+++ app/routes/services.js
@@ -3,3 +3,4 @@
 // place routes in the this file.  Obv be careful on order.  Not just
 // in this file but for other registered route files in the express config
 
+const svcs = require('../controllers/services.js')
